1628994e62cd7341ecdfe4e1cfa97230845a438d
Enrique Morato <quiquemorato@gmail.com>
2022-06-07 09:28:00 +0200

    Update main app component to get views

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import ProductsList from './views/ProductsList/ProductsList'
+import ProductDetails from './views/ProductDetails/ProductDetails'
+import './App.scss';
+import { Route } from 'wouter';
+import Header from './components/Header/Header';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <Route path="/" component={() => <ProductsList />}/>     
+      <Route path="/product/:id" component={() => <ProductDetails />}/>
     </div>
   );
 }
